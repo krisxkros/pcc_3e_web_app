0fb4f8caeb40b4bb21dbb9c87cf5e545d838f0fb
krisxkros <kris836@gmail.com>
2024-11-12 14:59:37 +0100

    Starting an app section finished.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # The dot in front of models tells Django to look for models.py in the same directory as admin.py.
-from .models import Topic
+from .models import Topic, Entry
 
 # Register your models here.
 
 # Tells Django to manage our model through the admin site:
 admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,6 +6,23 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    """Return a string representation of a model."""
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    # 'ForeignKey: connect each entry with a particular topic
+    # 'on_delete=models.CASCADE': when a topic is deleted, all the entries associated with that topic should be deleted as well. This is known as a 'cascading delete'.
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        if len(self.text) >= 50:
+            return f"{self.text[:50]}..."
+        return f"{self.text}"
