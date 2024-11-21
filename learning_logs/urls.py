2ae9392036e94850b880ddccda02f7fd05208bfe
krisxkros <kris836@gmail.com>
2024-11-21 12:27:35 +0100

    Homepage URLs, views, templates added.

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""Defines URL patterns for learning logs."""
+
+from django.urls import path
+# the dot (.) tells Python to import the views.py module from the same directory
+# as the current urls.py module
+from . import views
+
+# `app_name`` helps Django distinguish this urls.py file from files of
+# the same name in other apps within the project
+app_name = "learning_logs"
+# `urlpatterns` in this module is a list of individual pages that can be
+# requested from the `learning_logs` app
+urlpatterns = [
+    # Home page
+    path("", views.index, name="index")
+]
