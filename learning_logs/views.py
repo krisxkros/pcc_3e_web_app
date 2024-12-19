0110e7e0b59134de9eca4dcc2c2007c8749bf185
krisxkros <kris836@gmail.com>
2024-12-19 12:23:19 +0100

    Minor changes.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,8 +22,8 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user:
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request.user)
+    
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -73,8 +73,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request.user)
 
     if request.method != "POST":
         # Initial request; pre-fill from with current entry
@@ -88,3 +87,8 @@ def edit_entry(request, entry_id):
 
     context = {"entry": entry, "topic": topic, "form": form}
     return render(request, "learning_logs/edit_entry.html", context)
+
+def check_topic_owner(topic, user):
+    """Check if topic matches the currently logged-in user."""
+    if topic.owner != user:
+        raise Http404
